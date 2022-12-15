6f297a988602c5dc2ca33271e592b88e07462210
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-15 21:28:47 +0000

    chore: Add custom styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,22 @@ module.exports = {
   theme: {
     purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
     safelist: ["primary-clr", "secondary-clr", "teriary-clr"],
+    fontFamily: {
+      steelfish: ["steelfish", "sans-serif"],
+    },
     extend: {
-      // colors: {
-      //   "primary-clr": "var(--primary-clr)",
-      //   "secondary-clr": "var(--secondary-clr)",
-      //   "teriary-clr": "var(--teriary-clr)",
-      // },
+      colors: {
+        "primary-clr": "var(--primary-clr)",
+        "secondary-clr": "var(--secondary-clr)",
+        "teriary-clr": "var(--teriary-clr)",
+      },
+
+      screens: {
+        mini: { max: "450px" },
+        // => @media (max-width: 450px) { ... }
+        xxl: { min: "2000px" },
+        // => @media (max-width: 1950px) { ... }
+      },
     },
   },
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
