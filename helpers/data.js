9c90aa065d4908c9dc8e3444c214adf303b78ccc
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-16 23:03:21 +0000

    chore: Change images in data

--- helpers/data.js
+++ helpers/data.js
@@ -1,34 +1,81 @@
 export const projectList = [
   {
     title: "Cutting Edge Project",
-    image: "https://source.unsplash.com/collection/347317/",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/319903721_2376488525853048_5419000301901099010_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iq6fdxqB0S8AX9NTxPO&_nc_ht=scontent.flhr6-1.fna&oh=00_AfB5IWy3Qhl8zl2a9nSAN17cNjEhhggi5zKGTkMEs6OAIw&oe=63A25495",
+    tiny: "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/319903721_2376488525853048_5419000301901099010_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iq6fdxqB0S8AX9NTxPO&_nc_ht=scontent.flhr6-1.fna&oh=00_AfB5IWy3Qhl8zl2a9nSAN17cNjEhhggi5zKGTkMEs6OAIw&oe=63A25495",
     description:
       "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Mauris consectetur pharetra metus eu suscipit. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. ",
   },
   {
     title: "Featured Artist 3D",
-    image: "https://source.unsplash.com/collection/3573299/",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/318431555_814473343309184_8680954521691650359_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WABWaTQnhxkAX-CexOK&_nc_ht=scontent.flhr6-1.fna&oh=00_AfBZhwH80tpTKXOiWJUDY9PikKcPhl1YCnS9OEMihc1Cpw&oe=63A10F32",
+    tiny: "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/318431555_814473343309184_8680954521691650359_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WABWaTQnhxkAX-CexOK&_nc_ht=scontent.flhr6-1.fna&oh=00_AfBZhwH80tpTKXOiWJUDY9PikKcPhl1YCnS9OEMihc1Cpw&oe=63A10F32",
     description:
       "Duis at tellus vitae velit aliquet varius. Fusce luctus libero et ligula tristique lobortis. Vestibulum eu placerat risus, eu semper augue. Integer at purus sit amet elit pretium viverra. Suspendisse id fringilla nibh, nec dictum urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. ",
   },
   {
     title: "Interactive AI",
-    image: "https://source.unsplash.com/collection/2276562/",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/316199823_800710114685507_2176919950454165038_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zWwJKtgxmM0AX-GWVkh&_nc_ht=scontent.flhr6-1.fna&oh=00_AfDiUU8eFnBWEfuEOhDwYqVtQkmfwHlf8cVYA73Z_Ngb6Q&oe=63A0F783",
+    tiny: "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/316199823_800710114685507_2176919950454165038_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zWwJKtgxmM0AX-GWVkh&_nc_ht=scontent.flhr6-1.fna&oh=00_AfDiUU8eFnBWEfuEOhDwYqVtQkmfwHlf8cVYA73Z_Ngb6Q&oe=63A0F783",
     description:
       "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. Proin rhoncus magna lectus, nec consequat augue ultricies eu.",
   },
   {
     title: "Slices of Sky",
-    image: "https://source.unsplash.com/collection/2203755/",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/313903983_787754485981070_1600795776630688284_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=anTV_kD03H4AX_cvKEV&_nc_ht=scontent.flhr6-1.fna&oh=00_AfAqTCxCGgg7oyMcsZI7DTH68qAkLVX4cnzEiLaw6W1U0g&oe=63A23A43",
+    tiny: "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/313903983_787754485981070_1600795776630688284_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=anTV_kD03H4AX_cvKEV&_nc_ht=scontent.flhr6-1.fna&oh=00_AfAqTCxCGgg7oyMcsZI7DTH68qAkLVX4cnzEiLaw6W1U0g&oe=63A23A43",
     description:
       "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Mauris consectetur pharetra metus eu suscipit. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. ",
   },
   {
     title: "Micro Worlds",
-    image: "https://source.unsplash.com/collection/573009/",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/313001409_780998456656673_2297933893104489558_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=bjAO3AwCULsAX-NBtQW&_nc_ht=scontent.flhr6-1.fna&oh=00_AfB202d7yC8nltdoPoNlOSe1tVA_91FtmrOqv4yKtwzJtQ&oe=63A187A7",
+    tiny: "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/313001409_780998456656673_2297933893104489558_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=bjAO3AwCULsAX-NBtQW&_nc_ht=scontent.flhr6-1.fna&oh=00_AfB202d7yC8nltdoPoNlOSe1tVA_91FtmrOqv4yKtwzJtQ&oe=63A187A7",
     description:
       "Duis at tellus vitae velit aliquet varius. Fusce luctus libero et ligula tristique lobortis. Vestibulum eu placerat risus, eu semper augue. Integer at purus sit amet elit pretium viverra. Suspendisse id fringilla nibh, nec dictum urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. ",
   },
 ];
 
-export default projectList;
+export const offerList = [
+  {
+    id: 1,
+    title: "Title1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vitae sapiente necessitatibus consequatur dolorum mollitia vero eveniet nulla aliquam magni.",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/300854558_1759223354421256_4845859852264147584_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=Nv2F611pumUAX_0z53H&_nc_ht=scontent.flhr6-1.fna&oh=00_AfB2bAtwA2DRdT7zCOtHUS2wScKrVss2_X4zqqkiWtc9LQ&oe=63A0CC87",
+    link: "link",
+  },
+  {
+    id: 2,
+    title: "Title2",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vitae sapiente necessitatibus consequatur dolorum mollitia vero eveniet nulla aliquam magni.",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/292165742_1724268144583444_309880858522910030_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=Oz9phXnckcYAX85cZZ7&_nc_ht=scontent.flhr6-1.fna&oh=00_AfAPwhyNblhi7rCz9FdZYpB4xE19bSRf71r9NNM4n4mQOw&oe=63A0FC13",
+    link: "link",
+  },
+  {
+    id: 3,
+    title: "Title3",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vitae sapiente necessitatibus consequatur dolorum mollitia vero eveniet nulla aliquam magni.",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/288131653_1709107996099459_7136673036089854972_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a26aad&_nc_ohc=lAKFn1V68FMAX_VCLnu&_nc_ht=scontent.flhr6-1.fna&oh=00_AfC9jnK9wTGWFHa7hkHHa-gtWgYnCT_kvzU4-J6BJbpccQ&oe=63A15A37",
+    link: "link",
+  },
+  {
+    id: 4,
+    title: "Title4",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vitae sapiente necessitatibus consequatur dolorum mollitia vero eveniet nulla aliquam magni.",
+    image:
+      "https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/284347270_1695924194084506_5228830735952217347_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=vq8lo2vYI6AAX9t28Dq&_nc_ht=scontent.flhr6-1.fna&oh=00_AfBVp-Lq5fsMS91ttXsK2BqPVIPltvf_BxGyrgGxiBPkcQ&oe=63A1D442",
+    link: "link",
+  },
+];
