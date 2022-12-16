29ba7e8ff8fe374580839be948a509967219f3a8
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-16 23:01:39 +0000

    chore : Change image

--- components/about/About.js
+++ components/about/About.js
@@ -2,7 +2,7 @@ import Image from "next/legacy/image";
 
 export default function About() {
   return (
-    <section className="relative my-24">
+    <section className="relative">
       <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
           <div className="relative z-10 lg:py-16">
@@ -10,7 +10,7 @@ export default function About() {
               <Image
                 objectFit="cover"
                 layout="fill"
-                src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                src="https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/293219047_1729850147358577_148018764642219458_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=9-LZ9q2R5gAAX_qoBR-&_nc_ht=scontent.flhr6-1.fna&oh=00_AfARUs5vmBXps144s0AUQ0DZRBYU8odnWFfGW5tDms3zzw&oe=63A1C7BE"
                 alt=""
               />
             </div>
@@ -20,9 +20,7 @@ export default function About() {
             <span className="hidden lg:absolute lg:inset-y-0 lg:-left-16 lg:block lg:w-16 lg:bg-[color:var(--teriary-clr)]"></span>
 
             <div className="p-8 sm:p-16 lg:p-12 xl:p-24">
-              <h2 className="text-xl font-bold font-steelfish sm:text-5xl uppercase text-gray-800 tracking-wider">
-                Welcome to Liva studio
-              </h2>
+              <h2 className="heading-styles">Welcome to Liva studio</h2>
 
               <p className="mt-4 text-gray-800">
                 A portrait and lifestyle photographer, based in New Zealand. My style is very candid
