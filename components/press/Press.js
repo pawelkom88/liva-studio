e874cc19872e036e7bf7072da984671f00351f2e
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-18 17:38:15 +0000

    chore: Edit styles

--- components/press/Press.js
+++ components/press/Press.js
@@ -6,13 +6,13 @@ export default function Press() {
   return (
     <section className="-mt-8 lg:mt-0 mb-12">
       <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-10 lg:gap-40 lg:grid-cols-2">
-          <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg order-2">
-            <div className="max-w-xl mb-6 text-right">
+        <div className="grid large:gap-20 gap-10 lg:grid-cols-2">
+          <div className="flex flex-col justify-center items-center md:pr-8 xl:pr-0 lg:max-w-lg order-1">
+            <div className="max-w-xl mb-6 text-left large:flex-center large:flex-col">
               <h4 className="max-w-lg mb-6 large:text-center heading-styles tracking-tight text-[color:var(--primary-clr)] sm:leading-loose">
                 OUR LATES ON INSTAGRAM
               </h4>
-              <p className="text-base large:text-left md:text-lg">
+              <p className="text-base large:text-center md:text-lg">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
                 doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
               </p>
@@ -28,19 +28,19 @@ export default function Press() {
               </svg>
             </div>
           </div>
-          <div className="flex-center">
+          <div className="flex-center order-2">
             <div className="flex flex-col items-end ">
               <Image
                 width={160}
                 height={192}
-                className="relative -mr-8 object-cover mb-6 rounded shadow-lg h-48 xl:h-56 w-48 xl:w-56 z-10"
+                className="relative -mr-8 object-cover mb-6 rounded shadow-lg h-48 xl:h-72 w-48 xl:w-72 z-10"
                 src="https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/278243342_1667298410280418_7852507031280963432_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=SI6Y_kioZuMAX-jMG5k&_nc_ht=scontent.flhr6-1.fna&oh=00_AfDO2gsmW_0L35SSFIhZAta39SKUsp45DUtj_AA0Fmfe0Q&oe=63A2660D"
                 alt=""
               />
               <Image
-                width={128}
-                height={128}
-                className="-mr-6 object-cover  rounded shadow-lg h-32 xl:h-40 w-32 xl:w-40"
+                width={228}
+                height={228}
+                className="-mr-6 object-cover  rounded shadow-lg h-32 xl:h-44 w-32 xl:w-44"
                 src="https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/292083784_1724267224583536_286574393802491749_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=pu9MeKkIlZAAX-QP07r&_nc_ht=scontent.flhr6-1.fna&oh=00_AfCAmxma8767Wf811eiCSkepIxzEBvEfDTvrdGNdr0NTAA&oe=63A2B80D"
                 alt=""
               />
@@ -49,7 +49,7 @@ export default function Press() {
             <Image
               width={256}
               height={256}
-              className="object-cover rounded shadow-lg h-64 xl:h-80 w-64 xl:w-80"
+              className="object-cover rounded shadow-lg h-64 xl:h-96 w-64 xl:w-96"
               src="https://scontent.flhr6-1.fna.fbcdn.net/v/t39.30808-6/289968917_1718155045194754_8451991120748487385_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=lX7pbW1NqPEAX-3NSgN&_nc_ht=scontent.flhr6-1.fna&oh=00_AfBJwbTIRP1D5XJVe74_Zj1RUD_c-mxnf6KUZNuK1TvIEA&oe=63A34CC9"
               alt=""
             />
