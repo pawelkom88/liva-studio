e6638843536c7ee237ea9e26c0fef13de8ef845a
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-18 17:40:22 +0000

    chore: Conditionally display Offer components

--- components/our-offer/OurOffer.js
+++ components/our-offer/OurOffer.js
@@ -1,12 +1,19 @@
 import Offer from "../offer/Offer";
+import OfferReversed from "../offer/OfferReversed";
 import { offerList } from "@helpers/data.js";
 
 export default function OurOffer() {
   return (
-    <section className='my-24'>
+    <section className="my-24">
       <h4 className="heading-styles text-center">OUR OFFER</h4>
-      {offerList.map((offer,idx) => (
-        <Offer key={offer.id} details={offer} idx={idx + 2}/>
+      {offerList.map((offer, idx) => (
+        <div key={offer.id}>
+          {idx % 2 === 0 ? (
+            <Offer details={offer} />
+          ) : (
+            <OfferReversed key={offer.id} details={offer} />
+          )}
+        </div>
       ))}
     </section>
   );
