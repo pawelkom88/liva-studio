eb370f4de4b656a45e9cae0afd7f984f224940dd
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-18 17:42:48 +0000

    feat: Add offer page and styles

---
+++ pages/offer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Wrapper from "@components/wrapper/Wrapper";
+
+export default function offer() {
+  return <Wrapper>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, quia?</Wrapper>;
+}
