4cfd37fb7cbedcb4035948d510119987d395836f
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-18 17:37:19 +0000

    chore: Add href prop

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Button({ children, styles }) {
+export default function Button({ children, styles, href }) {
   return (
     <Link
-      href="#"
+      href={href}
       className={`${styles?.background} ${styles?.border} ${styles?.textHover} mt-8 inline-block  px-12 py-3 text-sm text-white font-bold hover:bg-transparent border hover:border  focus:outline-none focus:ring active:text-indigo-500 uppercase`}>
       {children}
     </Link>
