5ecac90ac7268a5008150da3cea42231ab52124c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-19 10:50:05 +0000

    chore: Add categories to data objects

--- helpers/images.js
+++ helpers/images.js
@@ -4,7 +4,7 @@ export const images = [
     original: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
     width: 320,
     height: 174,
-
+    category: "Maternity",
     caption: "After Rain (Jeshu John - designerspics.com)",
   },
   {
@@ -12,6 +12,7 @@ export const images = [
     original: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
     width: 320,
     height: 212,
+    category: "Maternity",
     caption: "Boats (Jeshu John - designerspics.com)",
   },
   {
@@ -19,6 +20,7 @@ export const images = [
     original: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
     width: 320,
     height: 212,
+    category: "Maternity",
     caption: "Color Pencils (Jeshu John - designerspics.com)",
   },
   {
@@ -26,6 +28,7 @@ export const images = [
     original: "https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg",
     width: 320,
     height: 213,
+    category: "Newborn",
     caption: "Red Apples with other Red Fruit (foodiesfeed.com)",
   },
   {
@@ -33,6 +36,7 @@ export const images = [
     original: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
     width: 320,
     height: 183,
+    category: "Newborn",
     caption: "37H (gratispgraphy.com)",
   },
   {
@@ -40,6 +44,7 @@ export const images = [
     original: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
     width: 240,
     height: 320,
+    category: "Boudoir",
 
     caption: "8H (gratisography.com)",
   },
@@ -48,6 +53,7 @@ export const images = [
     original: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
     width: 320,
     height: 190,
+    category: "Boudoir",
     caption: "286H (gratisography.com)",
   },
   {
@@ -55,6 +61,7 @@ export const images = [
     original: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
     width: 320,
     height: 148,
+    category: "Boudoir",
     caption: "315H (gratisography.com)",
   },
   {
@@ -62,6 +69,7 @@ export const images = [
     original: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
     width: 320,
     height: 213,
+    category: "Boudoir",
     caption: "201H (gratisography.com)",
   },
   {
@@ -70,6 +78,7 @@ export const images = [
     alt: "Big Ben - London",
     width: 248,
     height: 320,
+    category: "Boudoir",
     caption: "Big Ben (Tom Eversley - isorepublic.com)",
   },
   {
@@ -78,6 +87,7 @@ export const images = [
     alt: "Red Zone - Paris",
     width: 320,
     height: 113,
+    category: "Boudoir",
     caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
   },
   {
@@ -86,6 +96,7 @@ export const images = [
     alt: "Wood Glass",
     width: 313,
     height: 320,
+    category: "Wedding",
     caption: "Wood Glass (Tom Eversley - isorepublic.com)",
   },
   {
@@ -93,6 +104,7 @@ export const images = [
     original: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
     width: 320,
     height: 213,
+    category: "Wedding",
     caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
   },
   {
@@ -100,6 +112,7 @@ export const images = [
     original: "https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg",
     width: 320,
     height: 194,
+    category: "Wedding",
     caption: "Old Barn (Tom Eversley - isorepublic.com)",
   },
   {
@@ -108,6 +121,7 @@ export const images = [
     alt: "Cosmos Flower",
     width: 320,
     height: 213,
+    category: "Wedding",
     caption: "Cosmos Flower Macro (Tom Eversley - isorepublic.com)",
   },
   {
@@ -115,6 +129,7 @@ export const images = [
     original: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
     width: 271,
     height: 320,
+    category: "Wedding",
     caption: "Orange Macro (Tom Eversley - isorepublic.com)",
   },
   {
@@ -122,6 +137,7 @@ export const images = [
     original: "https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg",
     width: 320,
     height: 213,
+    category: "Wedding",
 
     caption: "Surfer Sunset (Tom Eversley - isorepublic.com)",
   },
@@ -130,6 +146,7 @@ export const images = [
     original: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
     width: 320,
     height: 213,
+    category: "Lifestyle",
 
     caption: "Man on BMX (Tom Eversley - isorepublic.com)",
   },
@@ -138,6 +155,7 @@ export const images = [
     original: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
     width: 320,
     height: 213,
+    category: "Lifestyle",
     caption: "Ropeman - Thailand (Tom Eversley - isorepublic.com)",
   },
   {
@@ -145,6 +163,7 @@ export const images = [
     original: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
     width: 320,
     height: 213,
+    category: "Lifestyle",
     caption: "Time to Think (Tom Eversley - isorepublic.com)",
   },
   {
@@ -152,6 +171,7 @@ export const images = [
     original: "https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg",
     width: 320,
     height: 179,
+    category: "Lifestyle",
 
     caption: "Untitled (Jan Vasek - jeshoots.com)",
   },
@@ -160,6 +180,7 @@ export const images = [
     original: "https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg",
     width: 320,
     height: 215,
+    category: "Lifestyle",
 
     caption: "Untitled (moveast.me)",
   },
@@ -168,6 +189,7 @@ export const images = [
     original: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
     width: 257,
     height: 320,
+    category: "Lifestyle",
     caption: "A photo by 贝莉儿 NG. (unsplash.com)",
   },
   {
@@ -175,6 +197,7 @@ export const images = [
     original: "https://c7.staticflickr.com/9/8824/28868764222_19f3b30773_b.jpg",
     width: 226,
     height: 320,
+    category: "Lifestyle",
     caption: "A photo by Matthew Wiebe. (unsplash.com)",
   },
 ];
