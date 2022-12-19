9c47c22ac6c6f472b2b9920b3841b3e564c67652
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-19 09:11:10 +0000

    chore: Add seo related stuff

---
+++ seo/seo.config.js
@@ -0,0 +1,28 @@
+export const SEO = {
+  openGraph: {
+    type: "website",
+    locale: "en_UK",
+    siteName: "Liva Studio - Photography website",
+  },
+  twitter: {
+    handle: "@aaa", // twitter:creator
+    site: "@bbb", // twitter:site
+    cardType: "@ccc", // twitter:card
+  },
+};
+
+export const additionalLinkTags = [
+  {
+    rel: "icon",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "apple-touch-icon",
+    href: "/apple-touch-icon.png",
+    sizes: "76x76",
+  },
+  {
+    rel: "manifest",
+    href: "/site.webmanifest",
+  },
+];
---
+++ seo/seo.js
@@ -0,0 +1,9 @@
+export const mainPageSeo = {
+  pageTitle: "Liva Studio",
+  pageDescription: "Website about photography",
+  canonical: "https://liva-studio.netlify.app/",
+  url: "https://liva-studio.netlify.app/",
+  OpenGraphTitle: "Liva Studio",
+  OpenGraphDescription: "Website about photography",
+  openGraphImageUrl: "https://liva-studio.netlify.app/",
+};
