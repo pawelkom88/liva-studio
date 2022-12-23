ca77e66abf89ef480f2931173feb908f391f456c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-23 10:59:44 +0000

    refactor: Remove unused code

--- pages/_app.js
+++ pages/_app.js
@@ -7,22 +7,15 @@ import "@fontsource/roboto";
 import { DefaultSeo } from "next-seo";
 import { SEO, additionalLinkTags } from "../seo/seo.config";
 
-import { AnimatePresence } from "framer-motion";
-import PageTransition from "@components/UI/page-transition/PageTransition";
-
 function MyApp({ Component, pageProps, router }) {
   const { openGraph, twitter } = SEO;
 
   return (
     <>
       <DefaultSeo openGraph={openGraph} twitter={twitter} additionalLinkTags={additionalLinkTags} />
-      <AnimatePresence exitBeforeEnter initial={true}>
-        <PageTransition transitionKey={router.route}>
-          <Nav />
-          <Component {...pageProps} />
-          <Footer />
-        </PageTransition>
-      </AnimatePresence>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
