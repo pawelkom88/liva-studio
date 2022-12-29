0b849007bc3886764175f8618af0773ed91ec629
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2022-12-29 22:10:14 +0100

    feat: Add carousel functionality and style the component

--- components/reviews/Reviews.js
+++ components/reviews/Reviews.js
@@ -1,58 +1,84 @@
+import { useState } from "react";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 import { reviews } from "@helpers/reviews";
+import SliderBtn from "@components/UI/slider-btn/SliderBtn";
 
 export default function Reviews() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  function nextSlide() {
+    setCurrentSlide(prevState => prevState + 1);
+
+    if (currentSlide === reviews.length - 1) {
+      setCurrentSlide(0);
+    }
+  }
+
+  function previousSlide() {
+    setCurrentSlide(prevState => prevState - 1);
+    console.log(currentSlide);
+    if (currentSlide === 0) {
+      setCurrentSlide(reviews.length - 1);
+    }
+  }
+
   return (
-    <section className="relative my-8">
-      <h2 className="heading-styles my-8 text-center">Reviews</h2>
-      <Swiper className="mySwiper">
-        {reviews.map(review => {
-          return (
-            <SwiperSlide key={review.id}>
-              <article className="border-2">
-                <div className="h-10 w-10">
-                  <Image
-                    width={10}
-                    height={10}
-                    className="h-10 w-10 rounded-full"
-                    src={review.image}
-                    alt={review.alt}
-                  />
-                </div>
-                <div className="mb-1 flex items-center">
-                  {review.stars?.map(star => {
+    <section className="bg-[color:var(--teriary-clr)] px-16">
+      <div className="mx-auto max-w-[1340px] px-4 sm:px-6 sm:py-24 md:py-16 lg:mr-0 lg:pl-8 lg:pr-0">
+        <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-3 lg:items-center lg:gap-x-16">
+          <article className="mx-auto max-w-xl text-center sm:text-left">
+            <h2 className="heading-styles text-center">Read reviews from our customers</h2>
+            <div className="lg:flex-center hidden lg:mt-8 lg:gap-4">
+              <SliderBtn onClick={previousSlide} aria="Previous slide" iconDirection="previous" />
+              <SliderBtn onClick={nextSlide} aria="Next slide" iconDirection="next" />
+            </div>
+          </article>
+          <div className="-mx-6 h-[275px] lg:col-span-2 lg:mx-0">
+            <blockquote className="flex h-full flex-col justify-between bg-white p-12 shadow-lg">
+              <div>
+                <div className="flex items-center justify-start gap-0.5">
+                  <div className="w-20">
+                    <Image
+                      width={50}
+                      height={50}
+                      className="rounded-full"
+                      src={reviews[currentSlide].image}
+                      alt={reviews[currentSlide].alt}
+                    />
+                  </div>
+                  {reviews[currentSlide].stars?.map(star => {
                     return (
                       <svg
                         key={star}
-                        aria-hidden="true"
-                        className="h-5 w-5 text-yellow-400"
-                        fill="currentColor"
+                        className="h-5 w-5"
+                        fill="gold"
                         viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
-                        <title>star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                       </svg>
                     );
                   })}
-
-                  <h3 className="ml-2 text-sm font-semibold text-gray-900 dark:text-white">
-                    {review.title}
-                  </h3>
                 </div>
-                <footer className="mb-5 text-sm text-gray-500 dark:text-gray-400">
-                  <p>
-                    Reviewed in the United Kingdom on{" "}
-                    <time dateTime={review.joined}>{review.joined}</time> by {review.name}
+                <div className="mt-4">
+                  <p className="text-2xl font-bold text-pink-600 sm:text-3xl">
+                    {reviews[currentSlide].title}
                   </p>
-                </footer>
-                <p className="mb-2 font-light text-gray-500 dark:text-gray-400">{review.content}</p>
-              </article>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+                  <p className="mt-4 italic leading-relaxed text-[color:var(--primary-clr)]">
+                    {reviews[currentSlide].content}
+                  </p>
+                </div>
+              </div>
+              <footer className="mt-8 text-sm text-gray-500">
+                &mdash; {reviews[currentSlide].name}
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="mt-8 flex justify-center gap-4 lg:hidden">
+          <SliderBtn onClick={previousSlide} aria="Previous slide" iconDirection="previous" />
+          <SliderBtn onClick={nextSlide} aria="Next slide" iconDirection="next" />
+        </div>
+      </div>
     </section>
   );
 }
