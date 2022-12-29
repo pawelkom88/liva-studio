6f09ad9ba3860b2fd1f51de9cdc29033fe335fe2
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2022-12-29 22:16:57 +0100

    refactor: refactor FAQ component

--- components/faq/Faq.js
+++ components/faq/Faq.js
@@ -1,34 +1,48 @@
-import FaqSuccessElement from "./faqSuccessElement";
-import FaqElement from "./FaqElement";
 import Image from "next/image";
-import { faqElements, faqImage } from "@helpers/data";
+import Link from "next/link";
 
 export default function Faq() {
   return (
     <article>
       <h5 className="heading-styles text-center">HOW IT WORKS ?</h5>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-6 row-gap-10 lg:grid-cols-2">
-          <div className="relative flex items-center bg-[color:var(--teriary-clr)] p-4">
-            <span className="hidden lg:absolute lg:inset-y-0 lg:-right-16 lg:block lg:w-16 lg:bg-[color:var(--teriary-clr)]"></span>
-            <div className="lg:py-6 lg:pr-16">
-              {faqElements.map(({ id, content }) => (
-                <FaqElement key={id} stepNumber={id} content={content} />
-              ))}
-              <FaqSuccessElement />
-            </div>
-          </div>
-          <div className="relative sm:h-96 my-auto">
+      <div className="mx-auto grid grid-cols-1 gap-8 px-4 py-16 sm:max-w-xl sm:grid-cols-3 md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20">
+        <div className="flex-center flex-col">
+          <strong className="my-2">Tell us what you need</strong>
+          <a
+            href="https://wedwojestudio.pl/oferta/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Our offer"
+            title="Our offer">
             <Image
-              width={596}
-              height={384}
-              className="inset-0 object-cover w-full rounded shadow-lg h-96 lg:absolute lg:h-full"
-              src={faqImage?.original}
-              placeholder="blur"
-              blurDataURL={faqImage?.tiny}
-              alt="faq image"
+              width={70}
+              height={70}
+              src="/assets/images/speech-bubble-with-emoticon.png"
+              alt="icon"
             />
-          </div>
+          </a>
+        </div>
+        <div className="flex-center flex-col">
+          <strong className="my-2">Book a date</strong>
+          <Link href="/contact">
+            <Image width={70} height={70} src="/assets/images/add-picture.png" alt="icon" />
+          </Link>
+        </div>
+        <div className="flex-center flex-col">
+          <strong className="my-2">Choose your photos</strong>
+          <a
+            href="https://wedwojestudio.pl/oferta/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Client space"
+            title="Client space">
+            <Image
+              width={70}
+              height={70}
+              src="/assets/images/calendar-page-with-upper-spring.png"
+              alt="icon"
+            />
+          </a>
         </div>
       </div>
     </article>
