19024854cf1a91288d84d704b5373c26855b4ace
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-27 22:13:59 +0000

    refactor: Extract code to a separate components

---
+++ components/hero/Hero.js
@@ -0,0 +1,14 @@
+export default function Hero({ heading, children, heroImg }) {
+  return (
+    <div className={`w-full bg-top bg-cover h-[38rem] lg:h-[calc(100vh-139px)] ${heroImg}`}>
+      <div className="flex items-center justify-center w-full h-full bg-gray-900/40">
+        <div className="text-center">
+          <h1 className="heading-styles text-white tracking-wider lg:text-6xl">{heading}</h1>
+          <p className="max-w-sm lg:max-w-2xl mt-4 text-white text-md lg:text-xl px-4">
+            {children}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
