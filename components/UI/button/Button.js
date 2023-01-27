fefb531b5082fb25d80d56556a55e94311997adc
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-27 22:15:26 +0000

    styles: Remove margin top

--- components/UI/button/Button.js
+++ components/UI/button/Button.js
@@ -4,7 +4,7 @@ export default function Button({ children, styles, href }) {
   return (
     <Link
       href={href}
-      className={`${styles?.background} ${styles?.border} ${styles?.textHover} mt-8 inline-block  px-12 py-3 text-sm text-white font-bold hover:bg-transparent border hover:border  focus:ring active:text-indigo-500 uppercase`}>
+      className={`${styles?.background} ${styles?.border} ${styles?.textHover} inline-block  px-12 py-3 text-sm text-white font-bold hover:bg-transparent border hover:border  focus:ring active:text-indigo-500 uppercase`}>
       {children}
     </Link>
   );
