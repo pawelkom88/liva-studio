30fbbe5aeed1668eb03fdd1c8da8b0c2dbd2b0af
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-29 21:11:53 +0000

    chore: Remove button

--- components/offer-image/OfferImage.js
+++ components/offer-image/OfferImage.js
@@ -1,4 +1,4 @@
-import Button from "@components/UI/button/Button";
+import Link from "next/link";
 import SkeletonLoader from "@components/UI/skeleton/Skeleton";
 
 const btnStyles = {
@@ -10,20 +10,18 @@ const btnStyles = {
 export default function OfferImage({ offer }) {
   if (!offer) return <SkeletonLoader />;
 
-  const { title, description, image, slug } = offer?.fields;
+  const { title, image, slug } = offer?.fields;
   const { url } = image?.fields?.file;
 
   return (
     <div className="relative h-96 overflow-hidden group">
       <div
         style={{ backgroundImage: `url(${"https:" + url})` }}
-        className={`h-full flex-center filter hover:scale-110 transition-all duration-1000 ease-in-out bg-no-repeat bg-cover bg-center`}>
-        <h2 className="heading-styles text-white">{title}</h2>
-        <div className="h-full opacity-0 group-hover:opacity-100 duration-1000 absolute inset-x-0 bottom-0 flex-center flex-col text-xl bg-[color:var(--teriary-clr)] text-gray-900 font-semibold z-20">
-          <Button href={`/offer/${slug}`} styles={btnStyles}>
-            Check it out
-          </Button>
-        </div>
+        className={`h-full flex-center hover:scale-110 transition-all duration-1000 ease-in-out bg-no-repeat bg-cover bg-center`}>
+        <h2 className="heading-styles text-white bg-[var(--primary-clr)] p-2">{title}</h2>
+        <Link href={`/offer/${slug}`} styles={btnStyles}>
+          <div className="h-full opacity-0 group-hover:opacity-100 duration-1000 absolute inset-x-0 bottom-0 flex justify-end flex-col text-xl bg-[color:var(--teriary-clr)] text-gray-900 font-semibold z-20"></div>
+        </Link>
       </div>
     </div>
   );
