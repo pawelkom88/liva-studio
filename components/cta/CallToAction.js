ddaff81738c5fafd8c249428545770d829b4ce82
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-29 20:56:03 +0000

    chore: Swap Link to button

--- components/cta/CallToAction.js
+++ components/cta/CallToAction.js
@@ -1,5 +1,3 @@
-import Button from "@components/UI/button/Button";
-
 const btnStyles =
   "hover:text-[color:var(--primary-clr)] border-[color:var(--primary-clr)] bg-[color:var(--primary-clr)] mt-8 inline-block px-12 py-3 text-sm text-white font-bold hover:bg-transparent border hover:border  focus:ring active:text-indigo-500 uppercase";
 
@@ -11,9 +9,9 @@ export default function CallToAction() {
         I CAN&apos;T WAIT TO HEAR FROM YOU
       </h6>
       <p className="my-8 text-md lg:text-xl">I&apos;LL SHOW YOU HOW BEAUTIFUL THE WORLD IS</p>
-      <Button styles={btnStyles} href="/contact">
+      <button className={btnStyles} href="/contact">
         get in touch
-      </Button>
+      </button>
     </section>
   );
 }
