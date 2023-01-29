7e21a232982e4397eeda59187327eda09155a706
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-29 20:56:34 +0000

    chore: Add dynamic background image

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,6 +1,8 @@
-export default function Hero({ heading, children, heroImg }) {
+export default function Hero({ heading, children, heroImg = "", backgroundImage = " " }) {
   return (
-    <div className={`w-full bg-top bg-cover h-[38rem] lg:h-[calc(100vh-139px)] ${heroImg}`}>
+    <div
+      style={{ backgroundImage: backgroundImage }}
+      className={`w-full aspect-video bg-no-repeat bg-center bg-cover h-[38rem] lg:h-[calc(100vh-139px)] ${heroImg}`}>
       <div className="flex items-center justify-center w-full h-full bg-gray-900/40">
         <div className="text-center">
           <h1 className="heading-styles text-white tracking-wider lg:text-6xl">{heading}</h1>
