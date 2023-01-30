994ee11a7742627410a6ffaf838f4b2de912d929
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-30 11:08:48 +0000

    chore: Add buttons and styles

--- components/pricing/PricingCard.js
+++ components/pricing/PricingCard.js
@@ -2,17 +2,25 @@ import Image from "next/image";
 import ListItem from "./list-item/ListItem";
 
 export default function PricingCard({ offerPackage }) {
+  const goldPackage = offerPackage.name === "Gold Package";
+
+  const btnStyles = `${
+    goldPackage
+      ? "bg-white text-[var(--primary-clr)] hover:bg-transparent hover:border-white hover:text-white"
+      : "bg-[var(--secondary-clr)] text-white hover:bg-transparent hover:border-[var(--primary-clr)] hover:text-[var(--primary-clr)]"
+  } w-1/2 mx-auto px-8 py-3 font-semibold relative overflow-hidden border-2 border-transparent`;
+
   return (
     <article
-      className={`flex flex-col min-h-[35rem] shadow-lg mt-4 ${
-        offerPackage.name === "Gold Package"
+      className={`flex flex-col min-h-[45rem] shadow-lg mt-4 ${
+        goldPackage
           ? "bg-[var(--primary-clr)] text-white transform lg:-translate-y-8"
           : "text-[var(--primary-clr)]"
       }`}>
       <div className="flex flex-col p-6 space-y-6 sm:p-8">
         <div className="space-y-2">
           <h2 className="text-2xl font-bold text-center uppercase">{offerPackage.name}</h2>
-          {offerPackage.name === "Gold Package" && (
+          {goldPackage && (
             <Image
               className="mx-auto pt-4"
               width={75}
@@ -25,10 +33,18 @@ export default function PricingCard({ offerPackage }) {
         <strong className="mt-3 leading-relaxed text-center text-5xl">£{offerPackage.price}</strong>
         <ul className="flex-1 mb-6">
           {offerPackage.content?.map((item, index) => {
-            return <ListItem key={index} details={item} iconColor={offerPackage.name}/>;
+            return <ListItem key={index} details={item} iconColor={offerPackage.name} />;
           })}
         </ul>
       </div>
+      <a href="tel:" className={btnStyles}>
+        {goldPackage && (
+          <span className="absolute top-0 right-0 px-4 py-1 text-[.45rem] tracking-wider text-center uppercase whitespace-no-wrap origin-bottom-left transform rotate-45 -translate-y-full translate-x-1/3 bg-[#ecc25d]">
+            Best value
+          </span>
+        )}
+        Book now
+      </a>
     </article>
   );
 }
