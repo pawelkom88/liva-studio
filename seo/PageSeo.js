802ef2aefdac48cc0068f046bfadcd8579b5635f
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2023-04-17 22:06:41 +0100

    add verification metatag

--- seo/PageSeo.js
+++ seo/PageSeo.js
@@ -11,26 +11,26 @@ const defaultSEO = {
 };
 
 export default function PageSeo({ seo = defaultSEO }) {
-
-  console.log(seo.title)
+  console.log(seo.title);
 
   const jsonData = {
     "@context": "http://schema.org",
     "@type": "LocalBusiness",
-    "name": "https://livastudio.co.uk/",
-    "description": "Based in Bristol, England, Liva Studio is a photography brand specialising in newborn, maternity, and wedding photography. We’re obsessed with documenting and preserving life’s special moments by combining beautiful pictures and cutting-edge technology to create blissful memories for our clients.",
-    "image": "https://livastudio.co.uk/_next/image?url=%2Fassets%2Fimages%2Flogo.jpg&w=96&q=75",
-    "logo": "https://livastudio.co.uk/_next/image?url=%2Fassets%2Fimages%2Flogo.jpg&w=96&q=75",
-    "url": "https://livastudio.co.uk/",
-    "sameAs": ["https://www.facebook.com/livastudio1/", "https://www.instagram.com/livastudios/"],
-    "address": {
+    name: "https://livastudio.co.uk/",
+    description:
+      "Based in Bristol, England, Liva Studio is a photography brand specialising in newborn, maternity, and wedding photography. We’re obsessed with documenting and preserving life’s special moments by combining beautiful pictures and cutting-edge technology to create blissful memories for our clients.",
+    image: "https://livastudio.co.uk/_next/image?url=%2Fassets%2Fimages%2Flogo.jpg&w=96&q=75",
+    logo: "https://livastudio.co.uk/_next/image?url=%2Fassets%2Fimages%2Flogo.jpg&w=96&q=75",
+    url: "https://livastudio.co.uk/",
+    sameAs: ["https://www.facebook.com/livastudio1/", "https://www.instagram.com/livastudios/"],
+    address: {
       "@type": "PostalAddress",
-      "streetAddress": "Wallingford Rd",
-      "addressLocality": "Bristol",
-      "postalCode": "BS4 1SW",
-      "addressCountry": "United Kingdom"
-    }
-  }
+      streetAddress: "Wallingford Rd",
+      addressLocality: "Bristol",
+      postalCode: "BS4 1SW",
+      addressCountry: "United Kingdom",
+    },
+  };
 
   return (
     <>
@@ -62,13 +62,14 @@ export default function PageSeo({ seo = defaultSEO }) {
           siteName: "Liva Studio ",
         }}
       />
+      <meta
+        name="ahrefs-site-verification"
+        content="eed22d1fd101d26bac11c222d7faa28cfa82a8c07ebf38a0be8482d863c862ee"></meta>
       <Script
-      id="json"
+        id="json"
         key="structured-data"
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonData) }}
-      >
-      </Script>
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonData) }}></Script>
     </>
   );
 }
